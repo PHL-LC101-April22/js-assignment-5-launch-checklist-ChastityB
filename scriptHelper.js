f8cef4230a3c71d3d1ca40ed74131c28cf02b37c
Chas <paredeschastity@gmail.com>
2022-06-21 21:05:42 -0400

    Update scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,7 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    /*
@@ -17,11 +18,54 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    if (testInput ===''){
+        return 'Empty'
+    } else if (isNaN(testInput)){
+        return 'Not a number'
+    } else if (Number(testInput)){
+        return 'Is a number' 
+    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   let form = document.querySelector('testForm');
+   form.addEventListener('formSubmit', function(event){
+
+    if (validateInput(pilot)=='Empty' || validateInput(copilot)=='Empty' || validateInput(fuelLevel)=='Empty' || validateInput(cargoLevel)=='Empty'){
+        alert('All fields are required!');
+        event.preventDefault();
+    } else if (validateInput(pilot)=='Is a number' || validateInput(copilot)=='Is a number'){
+        alert('Input must NOT be a number!');
+        event.preventDefault();
+    } else if (validateInput(fuelLevel)=='Not a number' || validateInput(cargoLevel)=='Not a number'){
+        alert('Input must be a number!');
+        event.preventDefault();
+    } else {
+    document.getElementById('pilotStatus').innerHTML = `Pilot ${pilotNameInput.value} Ready`
+    document.getElementById('copilotStatus'.innetHTML) = `Co-pilot ${copilotNameInput.value} Ready`
+
+        if (fuelLevel.value < 10000){
+            document.getElementById('faultyItems').style.visibility = 'visible';
+            document.getElementById('fuelStatus').innerHTML = `There is not enough fuel for the journey`;
+            document.getElementById('launchStatus').innerHTML = 'Shuttle not ready for launch';
+            document.getElementById('launchStatus').style.color = 'red';
+        } else {
+            document.getElementById('launchStatus').style.color = 'green';
+            document.getElementById('launchStatus').innerHTML = 'Shuttle is ready for launch';
+        }
+
+        if (cargoLevel.value > 10000){
+            document.getElementById('faultyItems').style.visibility = 'visible';
+            document.getElementById('cargoStatus').innerHTML = 'There is too much mass for the shuttle to take off'
+            document.getElementById('launchStatus').innerHTML = 'Shuttle not ready for launch';
+            document.getElementById('launchStatus').style.color = 'red';
+        } else {
+            document.getElementById('launchStatus').style.color = 'green';
+            document.getElementById('launchStatus').innerHTML = 'Shuttle is ready for launch';
+        }
+    }
+
+   })
 }
 
 async function myFetch() {
@@ -35,6 +79,7 @@ async function myFetch() {
 
 function pickPlanet(planets) {
 }
+
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
